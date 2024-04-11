49ce1e9b5391bd639c1e9b19a3f19d71c66648c6
zyz <zyz@travelsky.com.cn>
2024-04-11 14:17:17 +0800

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,82 @@
+<script setup>
+import { onMounted, ref } from "vue"
+
+let a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+let sss = ref()
+
+onMounted(() => {
+  setTimeout(() => {
+    a.forEach(item => {
+      sss.value.style.transform = `translate(0,-${item * 10}%)`;
+
+    });
+  }, 500);
+})
+</script>
+<template>
+  <div class="kuang">
+    <div ref="sss" class="sssss">
+      <div v-for="(item, index) in a" :key="index" v-text="item"></div>
+    </div>
+  </div>
+  <div class="bar">
+    <div class="barnei">
+      <div v-for="(item, index) in a " :key="index">
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.bar {
+  height: 30px;
+  width: 300px;
+  border: 1px solid black;
+  border-radius: 20px;
+  display: flex;
+  align-items: center;
+
+}
+
+.barnei {
+  height: 28px;
+  margin-left: 10px;
+  width: 50%;
+  background: linear-gradient(to right, #ffd65c, #fd7d20);
+  display: flex;
+
+}
+
+.barnei>div {
+  width: 30px;
+  height: 30px;
+  border-right: 1px solid white;
+}
+
+.kuang {
+  width: 50px;
+  height: 50px;
+  background-color: aquamarine;
+  overflow: hidden;
+}
+
+.sssss {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  transition: all 4s linear;
+  transform: translate(0, 0);
+}
+
+/* .sssss:hover {
+  transform: translate(0, -300px);
+
+} */
+
+.kuang>div {
+  line-height: 50px;
+  width: 50px;
+
+}
+</style>
---
+++ src/views/shuiqiutu.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="baowei">
+        <div class="boxTop">
+            <div class="top">
+            </div>
+        </div>
+        <div class="donghua">
+            <div class="liudong">
+
+            </div>
+        </div>
+        <div class="boxbot">
+            <div class="bottom">
+            </div>
+        </div>
+    </div>
+</template>
+  
+<style>
+.baowei {
+    /* clear: both;
+    overflow: hidden; */
+    height: 150px;
+    width: 150px;
+}
+
+.boxTop {
+    height: 100px;
+    width: 300px;
+    position: relative;
+    overflow: hidden;
+
+
+}
+
+.donghua {
+    position: absolute;
+    width: 150px;
+    height: 150px;
+    border-radius: 100%;
+    overflow: hidden;
+    top: 7px;
+}
+
+.liudong {
+    animation: huadong 3s 100;
+    height: 20px;
+    width: 700px;
+    position: absolute;
+    background-image: linear-gradient(to right, red, green);
+}
+
+@keyframes huadong {
+    from {
+        left: 0px;
+    }
+
+    to {
+        left: -300px;
+
+    }
+}
+
+.boxbot {
+    width: 300px;
+    height: 50px;
+    position: relative;
+    overflow: hidden;
+
+}
+
+.top {
+    width: 150px;
+    height: 150px;
+    background-color: aqua;
+    border-radius: 100%;
+    position: absolute;
+}
+
+.bottom {
+    position: absolute;
+    width: 150px;
+    height: 150px;
+    background-color: rgb(35, 56, 56);
+    border-radius: 100%;
+    bottom: 0px;
+}
+</style>
+  
